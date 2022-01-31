a384980dd21bee54508f1fd2c2426c2e5b79f219
eulucasm <lucaspoletis@gmail.com>
2022-01-31 19:06:47 -0300

    connectComponents

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -6,11 +6,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.app.inflate
 import com.raywenderlich.android.creatures.model.Creature
+import kotlinx.android.synthetic.main.list_item_creature.view.*
 
 class CreatureAdapter(private val creatures: List<Creature>) :
     RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
-    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private lateinit var creature : Creature
 
+        fun bind(creature: Creature){
+            this.creature = creature
+            val context = itemView.context
+            itemView.creatureImage.setImageResource(context.resources.getIdentifier(creature.uri, null, context.packageName))
+            itemView.fullName.text = creature.firstName
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,7 +29,7 @@ class CreatureAdapter(private val creatures: List<Creature>) :
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        holder.bind(creatures[position])
     }
 
 
